574564015e4e02c08955f6367eb97c1c39c4c1f9
sabrina-here <iknowthissabrina@gmail.com>
2024-02-13 06:45:43 +0600

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ app.get("/", (req, res) => {
   res.send("tutor finder server side running");
 });
 
+// SQ71VHn1Mw1zcjQ1
+// sabTutor
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://sabTutor:SQ71VHn1Mw1zcjQ1@cluster0.jmebqdy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  console.log(err);
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("tutorRev").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const services = await serviceCollection.find({}).toArray();
+      res.send(services);
+    });
+    app.get("/homeServices", async (req, res) => {
+      const services = await serviceCollection.find({}).limit(3).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
 app.listen(port, () => {
   console.log("running ...");
 });
